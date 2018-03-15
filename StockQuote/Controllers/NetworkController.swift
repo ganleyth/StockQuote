b5e1f6bdd6f3fd419c98bfbb224eb065787e5c53
Tom <ganleyapps@gmail.com>
2018-03-14 21:25:50 -0700

    Add network controller functionality to perform a GET request.

--- StockQuote/Controllers/NetworkController.swift
+++ StockQuote/Controllers/NetworkController.swift
@@ -10,7 +10,17 @@ import Foundation
 
 class NetworkController {
     
-    static func url(byAdding params: [String: String]?, to baseURL: URL) -> URL? {
+    static let shared = NetworkController()
+    
+    lazy var session: SessionProtocol = URLSession.shared
+    
+    func performGETRequest(for url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        session.dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }.resume()
+    }
+    
+    func url(byAdding params: [String: String]?, to baseURL: URL) -> URL? {
         let queryItems = params?.map({ URLQueryItem(name: $0.key, value: $0.value) })
         var components = URLComponents(url: baseURL, resolvingAgainstBaseURL: false)
         components?.queryItems = queryItems
@@ -19,3 +29,9 @@ class NetworkController {
     }
     
 }
+
+protocol SessionProtocol {
+    func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: SessionProtocol {}
--- StockQuoteTests/Controllers/NetworkControllerTests.swift
+++ StockQuoteTests/Controllers/NetworkControllerTests.swift
@@ -18,7 +18,7 @@ class NetworkControllerTests: XCTestCase {
             "param2": "value2"
         ]
         
-        guard let url = NetworkController.url(byAdding: params, to: baseURL),
+        guard let url = NetworkController.shared.url(byAdding: params, to: baseURL),
             let components = URLComponents(url: url, resolvingAgainstBaseURL: false) else { XCTFail(); return }
         
         let returnedHost = components.host
@@ -30,6 +30,66 @@ class NetworkControllerTests: XCTestCase {
         XCTAssertEqual(returnedHost, "testURL.com")
         XCTAssertEqual(returnedParams["param1"], "value1")
         XCTAssertEqual(returnedParams["param2"], "value2")
+    }
+    
+    func test_WhenIssuedGETRequest_DataAndErrorAreHandled() {
+        let bundle = Bundle(for: type(of: self))
+        guard let sampleDataPath = bundle.path(forResource: "SampleBatchQuotesJSON", ofType: "json"),
+            let url = URL(string: "https://testURL.com") else { XCTFail(); return }
+        
+        let data = try? Data(contentsOf: URL(fileURLWithPath: sampleDataPath))
+
+        let session = MockURLSession(data: data, urlResponse: nil, error: NSError(domain: "test", code: 99, userInfo: nil))
+        NetworkController.shared.session = session
+        
+        let expectation = XCTestExpectation()
+        NetworkController.shared.performGETRequest(for: url) { (responseData, responseError) in
+            XCTAssertEqual(data, responseData)
+            XCTAssertEqual((responseError as NSError?)?.code, 99)
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+}
+
+extension NetworkControllerTests {
+    
+    class MockURLSession: SessionProtocol {
+        
+        private let dataTask: MockTask
+        
+        init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+            self.dataTask = MockTask(data: data, urlResponse: urlResponse, error: error)
+        }
+        
+        func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            
+            dataTask.completionHandler = completionHandler
+            return dataTask
+        }
+    }
+    
+    class MockTask: URLSessionDataTask {
+        
+        private let data: Data?
+        private let urlResponse: URLResponse?
+        private let responseError: Error?
+        
+        var completionHandler: ((Data?, URLResponse?, Error?) -> Void)?
+        
+        init(data: Data?, urlResponse: URLResponse?, error: Error?) {
+            self.data = data
+            self.urlResponse = urlResponse
+            self.responseError = error
+        }
+        
+        override func resume() {
+            DispatchQueue.main.async { [weak self] in
+                guard let this = self else { XCTFail(); return }
+                this.completionHandler?(this.data, this.urlResponse, this.responseError)
+            }
+        }
     }
     
 }
