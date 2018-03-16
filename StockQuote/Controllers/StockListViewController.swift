4fc4fbeed6508b93f9ac7b39e7902cd06a734bfb
Tom <ganleyapps@gmail.com>
2018-03-15 19:19:47 -0700

    Add a view controller for the list view.

---
+++ StockQuote/Controllers/StockListViewController.swift
@@ -0,0 +1,15 @@
+//
+//  StockListViewController.swift
+//  StockQuote
+//
+//  Created by Thomas Ganley on 3/15/18.
+//  Copyright © 2018 Thomas Ganley. All rights reserved.
+//
+
+import UIKit
+
+class StockListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+
+}
