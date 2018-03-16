1f3a0a275834ea0fb42ee07438be8451c7b56d26
Tom <ganleyapps@gmail.com>
2018-03-15 21:12:20 -0700

    Implement cellForRowAt

--- StockQuote/Controllers/StockListInteractor.swift
+++ StockQuote/Controllers/StockListInteractor.swift
@@ -15,6 +15,12 @@ class StockListInteractor: NSObject, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "stockCell") else { return UITableViewCell() }
+        
+        let quote = QuoteController.shared.quotes?[indexPath.row]
+        cell.textLabel?.text = quote?.symbol
+        cell.detailTextLabel?.text = String(format: "$%0.2f", arguments: [quote?.price ?? 0])
+        
+        return cell
     }
 }
--- StockQuoteTests/Controllers/StockListInteractorTests.swift
+++ StockQuoteTests/Controllers/StockListInteractorTests.swift
@@ -11,7 +11,9 @@ import XCTest
 
 class StockListInteractorTests: XCTestCase {
     
-    func test_InteractorUsesBatchQuotesDataForRowCount() {
+    lazy var sut = StockListInteractor()
+    
+    override func setUp() {
         let bundle = Bundle(for: type(of: self))
         guard let sampleDataPath = bundle.path(forResource: "SampleBatchQuotesJSON", ofType: "json"),
             let data = try? Data(contentsOf: URL(fileURLWithPath: sampleDataPath)) else { XCTFail(); return }
@@ -19,10 +21,34 @@ class StockListInteractorTests: XCTestCase {
         let quotes = batchQuotesResponse?.quotes
         
         QuoteController.shared.quotes = quotes
+    }
+    
+    func test_Interactor_UsesBatchQuotesDataForRowCount() {
+        XCTAssertEqual(sut.tableView(UITableView(), numberOfRowsInSection: 0), QuoteController.shared.quotes?.count)
+    }
+    
+    func test_Interactor_PopulatesCellWithStockSymbolAndPrice() {
+        guard let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "listVC") as? StockListViewController else {
+            XCTFail()
+            return
+        }
+        vc.loadViewIfNeeded()
+        guard let tableView = vc.tableView else { XCTFail(); return }
+        let cell0 = sut.tableView(tableView, cellForRowAt: IndexPath(row: 0, section: 0))
+        let cell1 = sut.tableView(tableView, cellForRowAt: IndexPath(row: 1, section: 0))
+        let cell2 = sut.tableView(tableView, cellForRowAt: IndexPath(row: 2, section: 0))
         
-        let interactor = StockListInteractor()
+        XCTAssertEqual(cell0.textLabel?.text, QuoteController.shared.quotes?[0].symbol)
+        XCTAssertEqual(cell1.textLabel?.text, QuoteController.shared.quotes?[1].symbol)
+        XCTAssertEqual(cell2.textLabel?.text, QuoteController.shared.quotes?[2].symbol)
         
-        XCTAssertEqual(interactor.tableView(UITableView(), numberOfRowsInSection: 0), QuoteController.shared.quotes?.count)
+        let displayPrice0 = String(format: "$%0.2f", arguments: [(QuoteController.shared.quotes?[0].price ?? 0)])
+        let displayPrice1 = String(format: "$%0.2f", arguments: [(QuoteController.shared.quotes?[1].price ?? 0)])
+        let displayPrice2 = String(format: "$%0.2f", arguments: [(QuoteController.shared.quotes?[2].price ?? 0)])
+        
+        XCTAssertEqual(cell0.detailTextLabel?.text, displayPrice0)
+        XCTAssertEqual(cell1.detailTextLabel?.text, displayPrice1)
+        XCTAssertEqual(cell2.detailTextLabel?.text, displayPrice2)
     }
     
 }
