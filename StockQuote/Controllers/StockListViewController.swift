3bfe7f9b52d4f17a6419c916da14f3c42f8426fc
Tom <ganleyapps@gmail.com>
2018-03-15 21:21:55 -0700

    Set the data source of the table view and fetch data.

--- StockQuote/Controllers/StockListViewController.swift
+++ StockQuote/Controllers/StockListViewController.swift
@@ -11,5 +11,19 @@ import UIKit
 class StockListViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
-
+    let interactor = StockListInteractor()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = interactor
+        fetchStocks()
+    }
+    
+    private func fetchStocks() {
+        QuoteController.shared.fetchBatchQuotes(forSymbols: ["NKE", "GOOG", "MSFT", "FB", "AAPL"]) { [weak self] in
+            guard let this = self else { return }
+            this.tableView.reloadData()
+        }
+    }
 }
